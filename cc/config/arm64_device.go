91d6c85c4a319ab7ce1a763cacfdd2376dbcd117
Miguel Martinez <slverwolf@gmail.com>
2018-01-27 20:37:18 -0600

    cc/config: Add cortex-a57 arch flags to cflags for arm64
    Change-Id: Id982dbd983581425c72192a8b2fdfb362bbc78b4

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -51,6 +51,8 @@ var (
 		"-fgcse-after-reload",
 		"-frerun-cse-after-loop",
 		"-frename-registers",
+		"-mcpu=cortex-a57+crc+crypto+fp+simd --param l1-cache-line-size=64 --param l1-cache-size=32 --param l2-cache-size=512",
+		"-fomit-frame-pointer",
 	}
 
 	arm64Ldflags = []string{
