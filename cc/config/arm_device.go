22548db186df1632a0cf3009113f2adbfb2b6c6a
Miguel Martinez <slverwolf@gmail.com>
2018-01-29 11:45:21 -0600

    cc/config: arm: hardcode arch/fpu values
    Change-Id: I94c8ee4a1e481d3672cc7b4128f3e9e2c173e5a9

--- cc/config/arm_device.go
+++ cc/config/arm_device.go
@@ -51,6 +51,8 @@ var (
 		"-fgcse-after-reload",
 		"-frerun-cse-after-loop",
 		"-frename-registers",
+		"-mcpu=cortex-a15 --param l1-cache-line-size=64 --param l1-cache-size=32 --param l2-cache-size=512",
+		"-mfpu=neon-fp-armv8",
 	}
 
 	armCppflags = []string{
