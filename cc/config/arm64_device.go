f2008a58e25cb9ca5d9adcfd84804f73dff33019
Miguel Martinez <slverwolf@gmail.com>
2018-02-07 08:56:56 -0600

    cc/config/arm64_device.go: convert a57 to a53
    Targetting a57 tends to max out our soc and causes some glitches, settings a53 here relieves most of that pressure and smooth's the rom out a bit.
    
    Change-Id: I2985003b422a90011bcd3a793dc7f29de661bb1a

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -51,7 +51,7 @@ var (
 		"-fgcse-after-reload",
 		"-frerun-cse-after-loop",
 		"-frename-registers",
-		"-mcpu=cortex-a57+crc+crypto+fp+simd --param l1-cache-line-size=64 --param l1-cache-size=32 --param l2-cache-size=512",
+		"-mcpu=cortex-a53+crc+crypto+fp+simd --param l1-cache-line-size=64 --param l1-cache-size=32 --param l2-cache-size=512",
 	}
 
 	arm64Ldflags = []string{
@@ -80,7 +80,7 @@ var (
 		"kryo": []string{
 			// Use the cortex-a57 cpu since some compilers
 			// don't support a Kryo specific target yet.
-			"-mcpu=cortex-a57",
+			"-mcpu=cortex-a53",
 		},
 	}
 
@@ -101,7 +101,7 @@ func init() {
 		"denver64")
 
 	// Clang supports specific Kryo targeting
-	replaceFirst(arm64ClangCpuVariantCflags["kryo"], "-mcpu=cortex-a57", "-mcpu=kryo")
+	replaceFirst(arm64ClangCpuVariantCflags["kryo"], "-mcpu=cortex-a53", "-mcpu=kryo")
 
 	pctx.StaticVariable("arm64GccVersion", arm64GccVersion)
 
